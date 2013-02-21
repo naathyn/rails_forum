8089ac3722b7176514063fe62b856c6fa8780a12
naathyn <nathan3k@gmail.com>
2013-02-21 01:17:16 -0600

    a few changes to the model and styles

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,14 +3,11 @@ class Board < ActiveRecord::Base
   belongs_to :category
 
   has_many :topics
+  has_many :comments, through: :topics
 
   validates_presence_of :category_id, :description, :name
 
-  def latest_topic
-    topics.sort_by(&:updated_at).last
-  end
-
-  def latest_topic_title
-    latest_topic.title
+  def latest_post
+    comments.sort_by(&:updated_at).last || topics.sort_by(&:updated_at).last
   end
 end
